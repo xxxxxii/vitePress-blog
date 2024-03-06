d2beb23dc73276b9bba7fe430b19ec5c1697800e
xxxxxii <1973329248@qq.com>
2024-03-06 17:01:57 +0800

    add Gitalk

--- .vitepress/theme/components/Comments.vue
+++ .vitepress/theme/components/Comments.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="gitalk-container"></div>
 </template>
+
 <script lang="ts" setup>
 import "gitalk/dist/gitalk.css";
 import Gitalk from "gitalk";
@@ -22,16 +23,17 @@ onContentUpdated(() => {
   }
   deleteChild(element);
   const gitalk = new Gitalk({
-    clientID: "a8430bf8a0464113ee24",
-    clientSecret: "df4cbf03c4ceca8a39076e6b7a0fb878e55534e2",
-    repo: "blog-comments",
-    owner: "clark-cui",
-    admin: ["clark-cui"],
+    clientID: "ef6e085c5f4e69c987d0",
+    clientSecret: "e9d02278a1295be94fc6a946e5e2ad778e5dd3bb",
+    repo: "gitalk-comments",
+    owner: "gitalk-comments",
+    admin: ["yulinZ"],
     id: location.pathname.substring(0, 50), // Ensure uniqueness and length less than 50
     language: "zh-CN",
-    distractionFreeMode: true, // Facebook-like distraction free mode
+    distractionFreeMode: false, // Facebook-like distraction free mode
   });
   gitalk.render("gitalk-container");
 });
 </script>
+
 <style scoped></style>
