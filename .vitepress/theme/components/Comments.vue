a72f96e930d849f2681a823c2198815b4216a3fd
xxxxxii <1973329248@qq.com>
2024-03-06 17:15:19 +0800

    Gitalk set

--- .vitepress/theme/components/Comments.vue
+++ .vitepress/theme/components/Comments.vue
@@ -25,9 +25,9 @@ onContentUpdated(() => {
   const gitalk = new Gitalk({
     clientID: "ef6e085c5f4e69c987d0",
     clientSecret: "e9d02278a1295be94fc6a946e5e2ad778e5dd3bb",
-    repo: "xxxxxii",
-    owner: "gitalk-comments",
-    admin: ["yulinZ"],
+    repo: "gitalk-comments",
+    owner: "xxxxxii",
+    admin: ["xxxxxii"],
     id: location.pathname.substring(0, 50), // Ensure uniqueness and length less than 50
     language: "zh-CN",
     distractionFreeMode: false, // Facebook-like distraction free mode
