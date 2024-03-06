0a99f70cc4647159de0d2a5fec322654af1059c6
xxxxxii <1973329248@qq.com>
2024-03-06 15:29:41 +0800

    first commit

---
+++ .vitepress/menu.ts
@@ -0,0 +1,18 @@
+export const nav = [
+    {
+        text: "🏡Blogs",
+        link: "/",
+    },
+    {
+        text: "🔖Tags",
+        link: "/tags",
+    },
+    {
+        text: "📃Archives",
+        link: "/archives",
+    },
+    {
+        text: "🔥RSS",
+        link: "https://clark-cui.top/feed.xml",
+    },
+]
---
+++ node_modules/.pnpm/vitepress-plugin-twoslash@0.10.0_typescript@5.3.3/node_modules/vitepress-plugin-twoslash/README.md
@@ -0,0 +1,9 @@
+# vitepress-plugin-twoslash
+
+Enable Twoslash support in VitePress.
+
+[Documentation](https://shikiji.netlify.app/packages/vitepress#twoslash)
+
+## License
+
+MIT
